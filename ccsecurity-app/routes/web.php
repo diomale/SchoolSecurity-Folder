f2d18e5ac3b7019428d2fe3e766c843dcaaae3bc
diomale <insane0225@gmail.com>
2026-01-21 10:26:51 +0800

    log in page for superadmin

--- ccsecurity-app/routes/web.php
+++ ccsecurity-app/routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\SuperAdminAuthController;
+
+Route::get('/superadmin/login', [SuperAdminAuthController::class, 'showLogin'])
+    ->name('superadmin.login');
+
+Route::post('/superadmin/login', [SuperAdminAuthController::class, 'login'])
+    ->name('superadmin.login.submit');
+
+Route::post('/superadmin/logout', [SuperAdminAuthController::class, 'logout'])
+    ->name('superadmin.logout');
+
+Route::middleware('auth:superadmin')->group(function () {
+    Route::get('/superadmin/dashboard', function () {
+        return view('superadmin.dashboard');
+    })->name('superadmin.dashboard');
+});
