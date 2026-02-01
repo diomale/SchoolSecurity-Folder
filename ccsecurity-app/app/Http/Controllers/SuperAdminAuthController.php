1fdc67269232082bf05e5c77420d65ff87959092
diomale <insane0225@gmail.com>
2026-02-01 21:43:32 +0800

    use web to add a admin by the Superadmin

--- ccsecurity-app/app/Http/Controllers/SuperAdminAuthController.php
+++ ccsecurity-app/app/Http/Controllers/SuperAdminAuthController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class SuperAdminAuthController extends Controller
 {
+    
+    public function dashboard()
+    {
+        
+        $admins = Admin::all(); 
+
+        return view('superadmin.dashboard', compact('admins'));
+    }
+
     public function showLogin()
     {
         return view('superadmin.login');
@@ -37,4 +48,32 @@ class SuperAdminAuthController extends Controller
         Auth::guard('superadmin')->logout();
         return redirect()->route('superadmin.login');
     }
+
+    public function storeAdmin(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:150',
+            'email' => 'required|string|email|max:155|unique:mysql_second.admins,email',
+            'password' => 'required|string|min:8|max:100'
+        ]);
+
+        Admin::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+            'status' => 'active',
+        ]);
+
+        return redirect()->route('superadmin.dashboard')
+            ->with('success', 'New Admin created successfully!');
+    }
+
+   /*
+    public function showTableAdmin($id)
+    {
+        $admin = Admin::findOrFail($id);
+        return view('superadmin.admin_details', compact('admin'));
+    }
+    */
 }
+    
