f1394902feade8f3923fb840541519eb4997079b
diomale <insane0225@gmail.com>
2026-02-01 21:47:10 +0800

    modified the dashboard of the admin

--- ccsecurity-app/resources/views/Superadmin/dashboard.blade.php
+++ ccsecurity-app/resources/views/Superadmin/dashboard.blade.php
@@ -1,8 +1,28 @@
-<h1>Super Admin Dashboard</h1>
+<div>
+    <h1>Super Admin Dashboard</h1>
 
-<p>Welcome, {{ auth('superadmin')->user()->name }}</p>
+    <p>Welcome, {{ auth('superadmin')->user()->name }}</p>
 
-<form method="POST" action="{{ route('superadmin.logout') }}">
-    @csrf
-    <button type="submit">Logout</button>
-</form>
+    <form method="POST" action="{{ route('superadmin.logout') }}">
+        @csrf
+        <button type="submit">Logout</button>
+    </form>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($admins as $admin)
+            <tr>
+                <td> {{ $admin->name }} </td>
+                <td> {{ $admin->email }} </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
