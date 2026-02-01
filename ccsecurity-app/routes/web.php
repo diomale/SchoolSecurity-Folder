2c49537acb4661f7e5748cac7295f1ed1f7c67aa
diomale <insane0225@gmail.com>
2026-02-01 21:47:49 +0800

    improved the web routes of the superadmin & admin

--- ccsecurity-app/routes/web.php
+++ ccsecurity-app/routes/web.php
@@ -1,29 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SuperAdminAuthController;
+use App\Http\Controllers\AdminController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-use App\Http\Controllers\SuperAdminAuthController;
+// --- SUPER ADMIN ROUTES --- //
+Route::prefix('superadmin')->group(function () {
 
-Route::get('/superadmin/login', [SuperAdminAuthController::class, 'showLogin'])
-    ->name('superadmin.login');
+    
+    Route::middleware('guest:superadmin')->group(function () {
+        Route::get('/login', [SuperAdminAuthController::class, 'showLogin'])->name('superadmin.login');
+        Route::post('/login', [SuperAdminAuthController::class, 'login'])->name('superadmin.login.submit');
+    });
 
-Route::post('/superadmin/login', [SuperAdminAuthController::class, 'login'])
-    ->name('superadmin.login.submit');
-
-Route::post('/superadmin/logout', [SuperAdminAuthController::class, 'logout'])
-    ->name('superadmin.logout');
-
-Route::middleware('auth:superadmin')->group(function () {
-    Route::get('/superadmin/dashboard', function () {
-        return view('superadmin.dashboard');
-    })->name('superadmin.dashboard');
+    
+    Route::middleware('auth:superadmin')->group(function () {
+        
+        
+        Route::get('/dashboard', [SuperAdminAuthController::class, 'dashboard'])->name('superadmin.dashboard');
+        
+        Route::post('/store-admin', [SuperAdminAuthController::class, 'storeAdmin'])->name('superadmin.storeAdmin');
+        Route::get('/admin/{id}', [SuperAdminAuthController::class, 'showTableAdmin'])->name('superadmin.admin.show');
+        
+        Route::post('/logout', [SuperAdminAuthController::class, 'logout'])->name('superadmin.logout');
+    });
 });
 
-use App\Http\Controllers\AdminController;
-
-Route::get('/admin/login', [AdminController::class, 'showAdminLogin'])
-    ->name('admin.login');
+// --- ADMIN ROUTES --- //
+Route::prefix('admin')->group(function () {
+    Route::get('/login', [AdminController::class, 'showAdminLogin'])->name('admin.login');
+    
+});
