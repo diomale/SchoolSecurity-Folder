306fb92174825479c978a7fe0613767e034b6de8
diomale <insane0225@gmail.com>
2026-02-07 20:21:14 +0800

    login in welcome page for dev

--- ccsecurity-app/resources/views/welcome.blade.php
+++ ccsecurity-app/resources/views/welcome.blade.php
@@ -8,6 +8,14 @@
 </head>
 <body>
     <h1>Columban College Inc, Security System</h1>
+    <hr>
+
+    <h1>SuperAdmin</h1>
+    <a href="{{ route('superadmin.login') }}">Log In</a>
+    <br>
+    <h1>Admin</h1>
+    <a href="{{ route('admin.login') }}">Log In</a>
+    
 
     
 </body>
