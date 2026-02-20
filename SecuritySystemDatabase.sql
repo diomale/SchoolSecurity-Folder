aee9e9d50cc18ddf647c90aca88616658dde0ea2
diomale <insane0225@gmail.com>
2026-02-20 16:10:45 +0800

    modify the database

--- SecuritySystemDatabase.sql
+++ SecuritySystemDatabase.sql
@@ -21,24 +21,6 @@ CREATE SCHEMA IF NOT EXISTS `securitysystemdatabase` DEFAULT CHARACTER SET utf8m
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `securitysystemdatabase` DEFAULT CHARACTER SET utf8mb3 ;
 USE `securitysystemdatabase` ;
-
--- -----------------------------------------------------
--- Table `securitysystemdatabase`.`secutity_guard_user`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `securitysystemdatabase`.`secutity_guard_user` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `fullname` VARCHAR(200) NULL,
-  `first_name` VARCHAR(150) NULL,
-  `last_name` VARCHAR(150) NULL,
-  `email` VARCHAR(150) NULL,
-  `password` VARCHAR(100) NULL,
-  `created_at` TIMESTAMP NULL,
-  `updated_at` TIMESTAMP NULL,
-  `status` VARCHAR(45) NULL,
-  `profile_picture` BLOB NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
 USE `securitysystemdatabase` ;
 
 -- -----------------------------------------------------
@@ -52,8 +34,6 @@ CREATE TABLE IF NOT EXISTS `securitysystemdatabase`.`admins` (
   `status` VARCHAR(145) NULL DEFAULT NULL,
   `created_at` TIMESTAMP NULL DEFAULT NULL,
   `updated_at` TIMESTAMP NULL DEFAULT NULL,
-  `phone_number` VARCHAR(45) NULL,
-  `profile_picture` BLOB NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 AUTO_INCREMENT = 10
@@ -74,7 +54,6 @@ CREATE TABLE IF NOT EXISTS `securitysystemdatabase`.`inside_user` (
   `status` VARCHAR(50) NULL DEFAULT NULL,
   `created_at` TIMESTAMP NULL DEFAULT NULL,
   `updated_at` TIMESTAMP NULL DEFAULT NULL,
-  `prifile_picture` BLOB NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 AUTO_INCREMENT = 14
@@ -86,20 +65,36 @@ DEFAULT CHARACTER SET = utf8mb3;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `securitysystemdatabase`.`outside_user` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NULL DEFAULT NULL,
+  `last_name` VARCHAR(45) NULL DEFAULT NULL,
+  `email` VARCHAR(45) NULL DEFAULT NULL,
+  `phone_number` VARCHAR(45) NULL DEFAULT NULL,
+  `password` VARCHAR(255) NULL DEFAULT NULL,
+  `profile_picture` VARCHAR(45) NULL DEFAULT NULL,
+  `status` VARCHAR(50) NULL DEFAULT NULL,
+  `created_at` TIMESTAMP NULL DEFAULT NULL,
+  `updated_at` TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 8
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `securitysystemdatabase`.`secutity_guard_user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `securitysystemdatabase`.`secutity_guard_user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `fullname` VARCHAR(200) NULL DEFAULT NULL,
   `first_name` VARCHAR(150) NULL DEFAULT NULL,
   `last_name` VARCHAR(150) NULL DEFAULT NULL,
   `email` VARCHAR(150) NULL DEFAULT NULL,
-  `phone_number` VARCHAR(150) NULL DEFAULT NULL,
-  `password` VARCHAR(150) NULL DEFAULT NULL,
-  `profile_picture` BLOB NULL DEFAULT NULL,
-  `status` VARCHAR(50) NULL DEFAULT NULL,
+  `password` VARCHAR(100) NULL DEFAULT NULL,
   `created_at` TIMESTAMP NULL DEFAULT NULL,
   `updated_at` TIMESTAMP NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `fullname_UNIQUE` (`fullname` ASC) VISIBLE,
-  UNIQUE INDEX `first_name_UNIQUE` (`first_name` ASC) VISIBLE,
-  UNIQUE INDEX `last_name_UNIQUE` (`last_name` ASC) VISIBLE)
+  `status` VARCHAR(45) NULL DEFAULT NULL,
+  `profile_picture` BLOB NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
