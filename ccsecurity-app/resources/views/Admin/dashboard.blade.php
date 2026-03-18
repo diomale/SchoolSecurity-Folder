74217a76cf8e0438aea1ba0302db4cc899771f1b
Charlize <charlize5432101@gmail.com>
2026-03-18 11:58:09 +0800

    added and fixed new css

--- ccsecurity-app/resources/views/Admin/dashboard.blade.php
+++ ccsecurity-app/resources/views/Admin/dashboard.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/AdminStyleFolder/admin_style_dashboard.css', 'resources/js/app.js'])
     <title>Admin Dashboard</title>
 </head>
 <body>
