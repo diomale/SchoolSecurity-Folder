3f011a0d14c2b207c4ce280bd839907fe3ed10ed
Diomale Romero <insane0225@gmail.com>
2026-03-21 14:42:32 +0800

    redo

--- ccsecurity-app/vite.config.js
+++ ccsecurity-app/vite.config.js
@@ -6,49 +6,36 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                // Base & App Level
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/css/welcome.css',
-
-                // Super Admin Portal
                 'resources/css/SuperadminStyleFolder/superadmin_style_login.css',
                 'resources/css/SuperadminStyleFolder/superadmin_style_dashboard.css',
                 'resources/css/SuperadminStyleFolder/superadmin_style_add_form.css',
                 'resources/css/SuperadminStyleFolder/superadmin_style_details.css',
                 'resources/css/SuperadminStyleFolder/superadmin_style_edit.css',
-
-                // Security Guard Portal
                 'resources/css/SecurityGuardStyleFolder/securityguard_style_login.css',
                 'resources/css/SecurityGuardStyleFolder/securityguard_style_dashboard.css',
-                'resources/css/SecurityGuardStyleFolder/securityguard_style_entrylogs.css',
                 'resources/css/SecurityGuardStyleFolder/securityguard_style_qr_status_management.css',
                 'resources/css/SecurityGuardStyleFolder/securityguard_style_scanner.css',
-                'resources/css/SecurityGuardStyleFolder/securityguard_style_shift_history.css',
-                'resources/css/SecurityGuardStyleFolder/securityguard_style_shift_management.css',
                 'resources/css/SecurityGuardStyleFolder/securityguard_style_shift_schedule.css',
-
-                // Visitor / Outside User Portal
+                'resources/css/SecurityGuardStyleFolder/securityguard_style_shift_management.css',
+                'resources/css/SecurityGuardStyleFolder/securityguard_style_entrylogs.css',
+                'resources/css/SecurityGuardStyleFolder/securityguard_style_shift_history.css',
                 'resources/css/OutsideUSerStyleFolder/outsideuser_style_dashboard.css',
-                'resources/css/OutsideUSerStyleFolder/outsideuser_style_login.css',
                 'resources/css/OutsideUSerStyleFolder/outsideuser_style_signup.css',
-                'resources/css/OutsideUSerStyleFolder/outsideuser_style_notifications.css',
+                'resources/css/OutsideUSerStyleFolder/outsideuser_style_login.css',
                 'resources/css/OutsideUSerStyleFolder/outsideruser_style_profile.css',
                 'resources/css/OutsideUSerStyleFolder/outsideruser_style_visit_history.css',
                 'resources/css/OutsideUSerStyleFolder/outsideruser_style_visit_request..css',
-
-                // Inside User Portal
-                'resources/css/InsideUserStyleFolder/insideuser_dashboard_style.css',
+                'resources/css/OutsideUSerStyleFolder/outsideuser_style_notifications.css',
+                'resources/css/InsideUserStyleFolder/insideuser_style_dashboard.css',
                 'resources/css/InsideUserStyleFolder/insideuser_style_login.css',
                 'resources/css/InsideUserStyleFolder/insideuser_style_user_profile.css',
-
-                // Admin Portal
-                'resources/css/AdminStyleFolder/admin_style_crud.css',
                 'resources/css/AdminStyleFolder/admin_style_dashboard.css',
                 'resources/css/AdminStyleFolder/admin_style_login.css',
                 'resources/css/AdminStyleFolder/admin_style_qrstatus_management.css',
                 'resources/css/AdminStyleFolder/admin_style_user_profile.css',
-                'resources/css/AdminStyleFolder/insideuser_style_add_form.css',
             ],
             refresh: true,
         }),
